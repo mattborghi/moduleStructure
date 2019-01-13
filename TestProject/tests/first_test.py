a456f2e376b6c276cfbb964b7607416504020796
mattborghi <borghi.matias@gmail.com>
2019-01-13 16:23:18 -0300

    added test folder

---
+++ TestProject/tests/first_test.py
@@ -0,0 +1,23 @@
+import unittest
+
+# for testing sublime command
+class TestHelloWorld(unittest.TestCase):
+
+    def setUp(self):
+        print("setUp")
+
+    def tearDown(self):
+        print("tearDown")
+
+    def test_hello_world(self):
+        self.assertTrue(True, "new hello world")
+
+
+# class TestFunctions(TestCase):
+#
+#     def test_foo(self):
+#         x = helloworld.foo(1)
+#         self.assertEqual(x, 2)
+
+if __name__ == '__main__':
+    unittest.main()
